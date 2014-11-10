bf2d0aaf039b4642f5f5eca372d95c562feb299f
Zebraholic <rctavares@rogers.com>
2014-11-10 17:03:01 -0500

    Creating a user model, controller and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   resources :products
+
+Rainforest::Application.routes.draw do
+  resources :products
+  resources :users, only: [:new, :create]
+end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
