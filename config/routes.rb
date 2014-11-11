85444c11efe3e6b7e70d7e80e0e37035102f0372
Zebraholic <rctavares@rogers.com>
2014-11-11 16:39:36 -0500

    Creating product and user review models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rainforest::Application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   resources :products
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
 
