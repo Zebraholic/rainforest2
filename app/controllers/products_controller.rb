a356d565e8eac7c649324dc10f26333cd59e85ae
Zebraholic <rctavares@rogers.com>
2014-11-24 16:35:01 -0500

    Added search function and related JAX resources.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,16 @@ private
 public
 
   def index
-    @products = Product.all
+    @products = if params[:search]
+      Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
+    else
+      Product.all
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
